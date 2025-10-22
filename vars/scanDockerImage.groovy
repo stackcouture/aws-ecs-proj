3e22b9556b130d95bbe27b25073420268db6ab09
Naveen <naveenramlu28@gmail.com>
2025-10-22 12:19:28 +0530

    #update the trivy with output

--- vars/scanDockerImage.groovy
+++ vars/scanDockerImage.groovy
@@ -1,16 +1,23 @@
 def call(String ecrUri, String imageTag) {
-    def outputFile = "trivy-scan-${imageTag}.txt"
-
+    
+    def textReport = "trivy-scan-${imageTag}.txt"
+    def htmlReport = "trivy-scan-${imageTag}.html"
+    
     try {
         sh """
+            mkdir -p contrib
+            curl -sSL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/html.tpl -o contrib/html.tpl
+
             echo "Scanning Docker image ${ecrUri}:${imageTag} with Trivy..."
-            trivy image -f table -o ${outputFile} ${ecrUri}:${imageTag}
-            cat ${outputFile}
+            trivy image -f table -o ${textReport} ${ecrUri}:${imageTag}
+            cat ${textReport}
+
+            trivy image -f template -o ${htmlReport} --template "@contrib/html.tpl" ${ecrUri}:${imageTag}
         """
     } catch (Exception e) {
         echo "Trivy scan failed: ${e.getMessage()}"
         currentBuild.result = 'UNSTABLE'
     }
 
-    archiveArtifacts artifacts: outputFile, onlyIfSuccessful: true
+    archiveArtifacts artifacts: "${textReport}, ${htmlReport}", onlyIfSuccessful: true
 }
