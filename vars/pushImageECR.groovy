c4b611e88c4ce34d94926519935f690b16c991f4
Naveen <naveenramlu28@gmail.com>
2025-10-22 17:38:19 +0530

    #update the ECR logic

--- vars/pushImageECR.groovy
+++ vars/pushImageECR.groovy
@@ -2,9 +2,12 @@ def call(String imageName, String imageTag, String awsAccountId, String region) 
     def ecrUri = "${awsAccountId}.dkr.ecr.${region}.amazonaws.com/ecs-test-repo:${imageTag}"
     echo "Logging in to AWS ECR..."
     sh """
-        aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${awsAccountId}.dkr.ecr.${region}.amazonaws.com
-        docker tag ${imageName}:${imageTag} ${ecrUri}
-        docker push ${ecrUri}
+        docker tag diwali-wishes:latest ${ecrUri}:${imageTag}
+        docker tag diwali-wishes:latest ${ecrUri}:latest
+
+        echo "Pushing images to ECR..."
+        docker push ${ecrUri}:${imageTag}
+        docker push ${ecrUri}:latest
     """
     return ecrUri
 }
